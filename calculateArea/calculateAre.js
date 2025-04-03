0e35f119e109694fa3ffbeae9a148f2491fc100e
pk-0312 <pratikshask2424@gmail.com>
2025-04-03 18:02:41 +0530

    Update calculateAre.js

--- calculateArea/calculateAre.js
+++ calculateArea/calculateAre.js
@@ -2,15 +2,6 @@ let length;
 let width;
 
 function calculateArea() {
-
-}
-
-function calculateArea() {
- length = parseFloat(document.getElementById('length').value);
- width = parseFloat(document.getElementById('width').value);
-}
-
-function calculateArea() {
  length = parseFloat(document.getElementById('length').value);
  width = parseFloat(document.getElementById('width').value);
 
